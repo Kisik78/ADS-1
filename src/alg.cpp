e425b7a038e391bdc261675461a34aef540ecf7d
Коньков Арсений <95272498+Kisik78@users.noreply.github.com>
2022-02-24 12:22:53 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -4,17 +4,43 @@
 
 
 bool checkPrime(uint64_t value) {
-// вставьте код функции
+  if (value > 1) {
+    for (int i = 2; i < value; i++)
+      if (value % i == 0)
+        return false;
+    return true;
+  }
+  else
+    return false;
 }
 
 uint64_t nPrime(uint64_t n) {
-// вставьте код функции
+  for (int i = 2; i < (n/2) + 1; i++)
+    if (n % i == 0)
+      return false;
+  return true;
+  
+  for (int j = 2; j < 100 + 1; j++)
+    if (nPrime(j))
+      return false;
 }
 
 uint64_t nextPrime(uint64_t value) {
-// вставьте код функции
+  for (int i = 2; i < (value/2) + 1; i++)
+    if (value % i == 0)
+      return false;
+  return true;
+  
+  while (!nextPrime(value))
+    value++;
 }
 
 uint64_t sumPrime(uint64_t hbound) {
-// вставьте код функции
+  uint64_t sum = 0;
+  for (int i = 2; i < hbound; i++) {
+    if (checkPrime(i)) {
+      sum += i;
+    }
+  }
+  return sum;
 }
